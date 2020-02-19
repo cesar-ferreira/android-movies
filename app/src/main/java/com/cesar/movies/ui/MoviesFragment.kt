a9fca1c3b63554943e2f3f31105086ce0b09b808
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-02-19 19:39:08 -0300

    adding navigation host and creating fragment movies

---
+++ app/src/main/java/com/cesar/movies/ui/MoviesFragment.kt
@@ -0,0 +1,33 @@
+package com.cesar.movies.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.cesar.movies.R
+
+class MoviesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MoviesFragment()
+    }
+
+    private lateinit var viewModel: MoviesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.movies_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MoviesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/cesar/movies/ui/MoviesViewModel.kt
@@ -0,0 +1,7 @@
+package com.cesar.movies.ui
+
+import androidx.lifecycle.ViewModel
+
+class MoviesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
